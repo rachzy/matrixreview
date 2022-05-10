5b0acdd90f81d57e03d514151f45bf149650c4a4
Murilo2nobel <murilo.nunesb2006@gmail.com>
2022-05-10 15:37:59 -0300

    Continuando a index

--- js/matrixRainEffect.js
+++ js/matrixRainEffect.js
@@ -7,11 +7,11 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // Setting up the letters
-var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+var letters = "01100101010101101010101100110010101010110101010110寿司推薦レシピ 巻き寿司、にぎりにも挑戦作って楽しい、食べてうれしい♪おもてなしにもぴったりです寿司推薦レシピ0110010101010110101010110011001010101011010101011001100101010101101010101100110010101010110101010110れしい♪おもてなしにもぴったりです寿司推薦レシピ 1,40110010101010110101010110011001010101011010101011001100101010101101010101100110010101010110101010110011001010101011010101011001100101010101101010101100110010101010110101010110お寿司のアイデア盛りだくさんちらし寿司や巻き寿司、にぎりにも挑戦作って楽しい、食べてうれしい♪おもてなしにもぴったりです0110010101010110101010110";
 letters = letters.split('');
 
 // Setting up the columns
-var fontSize = 10,
+var fontSize = 13,
     columns = canvas.width / fontSize;
 
 // Setting up the drops
@@ -29,7 +29,8 @@ function draw() {
     ctx.fillStyle = '#0f0';
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
     drops[i]++;
-    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+    if (drops[i] * fontSize < canvas.height && Math.random() > .95) {
+      ctx.fillText(text, i * fontSize, drops[i] * fontSize);
       drops[i] = 0;
     }
   }
